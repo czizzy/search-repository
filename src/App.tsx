eef3f97bca5e7b60aee0bab0bd923125f529b0ee
izzy.cz <izzy.cz@antgroup.com>
2023-01-08 18:45:55 +0800

    feat: init husky & lint-staged

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import { useState } from "react"
-import reactLogo from "./assets/react.svg"
-import "./App.css"
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <div className="App">
@@ -24,7 +24,7 @@ function App() {
       </div>
       <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
