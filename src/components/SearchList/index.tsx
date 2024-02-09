9350665f50cd4d6c4d458f6df7919be740c41892
zhuo.chen <zhuo.chen@mujin.co.jp>
2024-02-09 09:50:22 +0900

    refactor: extract ListItem

--- src/components/SearchList/index.tsx
+++ src/components/SearchList/index.tsx
@@ -8,39 +8,44 @@ type SearchListProps = {
   q: string;
 };
 
+const ListItem = (props: { item: SearchRepositoryResponse["items"][number] }) => {
+  const { item } = props;
+  const dateTimeFormat = new Intl.DateTimeFormat();
+  return (
+    <li role="listitem">
+      <div className="repo-title">
+        <a href={item.html_url} target="_blank" rel="noreferrer" role="link" tabIndex={0}>
+          {item.full_name}
+        </a>
+      </div>
+      <div className="repo-desc">{item.description}</div>
+      {item.stargazers_count ? (
+        <div className="repo-info">
+          <div className="repo-star">
+            <a href={`${item.html_url}/stargazers`} target="_blank" rel="noreferrer">
+              <Star /> {item.stargazers_count}
+            </a>
+          </div>
+          <div className="repo-update">
+            Updated on {dateTimeFormat.format(new Date(item.updated_at))}
+          </div>
+        </div>
+      ) : null}
+    </li>
+  );
+};
+
 export function SearchList(props: SearchListProps) {
   const { result, q } = props;
   if (isSearchRepositoryResponse(result)) {
     const { items } = result;
-    const dateTimeFormat = new Intl.DateTimeFormat();
     return items.length ? (
       <div className="search-list">
         <h2>{result.total_count} repository results</h2>
         <ul role="list">
-          {items.map((item) => {
-            return (
-              <li role="listitem" key={item.id}>
-                <div className="repo-title">
-                  <a href={item.html_url} target="_blank" rel="noreferrer" role="link" tabIndex={0}>
-                    {item.full_name}
-                  </a>
-                </div>
-                <div className="repo-desc">{item.description}</div>
-                {item.stargazers_count ? (
-                  <div className="repo-info">
-                    <div className="repo-star">
-                      <a href={`${item.html_url}/stargazers`} target="_blank" rel="noreferrer">
-                        <Star /> {item.stargazers_count}
-                      </a>
-                    </div>
-                    <div className="repo-update">
-                      Updated on {dateTimeFormat.format(new Date(item.updated_at))}
-                    </div>
-                  </div>
-                ) : null}
-              </li>
-            );
-          })}
+          {items.map((item) => (
+            <ListItem item={item} key={item.id} />
+          ))}
         </ul>
       </div>
     ) : (
